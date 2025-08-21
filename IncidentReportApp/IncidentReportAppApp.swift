7153385b4e4d2ca9c244a3d7d8468d6ee546a3fd
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2025-08-21 17:46:27 +0300

    Initial commit - IncidentReport app

---
+++ IncidentReportApp/IncidentReportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncidentReportAppApp.swift
+//  IncidentReportApp
+//
+//  Created by mohamed ramadan on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct IncidentReportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IncidentReportAppTests/IncidentReportAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IncidentReportAppTests.swift
+//  IncidentReportAppTests
+//
+//  Created by mohamed ramadan on 21/08/2025.
+//
+
+import Testing
+@testable import IncidentReportApp
+
+struct IncidentReportAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
