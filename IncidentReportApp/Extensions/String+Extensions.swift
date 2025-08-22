f33848d5bc8d15e70e323f0a9413008c8feda576
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2025-08-22 20:11:19 +0300

    string extensions file added.

---
+++ IncidentReportApp/Extensions/String+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  String+Extensions.swift
+//  IncidentReportApp
+//
+//  Created by mohamed ramadan on 22/08/2025.
+//
+
+extension String {
+    var trimmedString: String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
+
